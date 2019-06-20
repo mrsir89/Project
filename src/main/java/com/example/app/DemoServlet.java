2eb8ff5a8f6d0bd9a6d2a453b1fe9e112811f57c
KANGIL <mrsir89#gmail.com>
2019-06-20 19:19:48 +0900

    Simple
    simple 테스트 1

--- src/main/java/com/example/app/DemoServlet.java
+++ src/main/java/com/example/app/DemoServlet.java
@@ -1,9 +1,20 @@
-public class DemoServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package com.example.app;
+
+import javax.servlet.ServletException; // 서블릿 세션, 부모임.
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class DemoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Result</h1>");
     }
 }
